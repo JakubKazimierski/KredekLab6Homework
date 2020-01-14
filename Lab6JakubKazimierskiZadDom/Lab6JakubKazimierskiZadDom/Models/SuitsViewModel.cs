df47f942e2bcf601da8d5b93c0ab5c0e997651e0
Jakub <j.m.kazimierski@gmail.com>
2020-01-14 05:37:20 +0100

    first model class for internet shop with suits

---
+++ Lab6JakubKazimierskiZadDom/Lab6JakubKazimierskiZadDom/Models/SuitsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab6JakubKazimierskiZadDom.Models
+{
+    public class SuitsViewModel
+    {
+        public string Model { get; set; }
+        public string Producer { get; set; }
+        public decimal Price { get; set; }
+        public string Photo { get; set; }
+
+        public SuitsViewModel(string model, string producer, decimal price, string photo)
+        {
+            Model = model;
+            Producer = producer;
+            Price = price;
+            Photo = photo;
+        }
+    }
+}
