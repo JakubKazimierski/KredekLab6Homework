d4bf4b49215af077142fce1b798de83969a55ea7
Jakub <j.m.kazimierski@gmail.com>
2020-01-14 05:46:27 +0100

    2 new form models to use get and post request

---
+++ Lab6JakubKazimierskiZadDom/Lab6JakubKazimierskiZadDom/Models/WorkWithUsViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab6JakubKazimierskiZadDom.Models
+{
+    public class WorkWithUsViewModel
+    {
+        /// <summary>
+        /// imie
+        /// </summary>
+        public string FirstName { get; set; }
+        /// <summary>
+        /// nazwisko
+        /// </summary>
+        public string LastName { get; set; }
+        /// <summary>
+        /// uczelnia
+        /// </summary>
+        public string University { get; set; }
+        /// <summary>
+        /// opis
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
